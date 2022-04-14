f15cf1dcd9bec4d81ab286afa52521f28d0aa9a2
eduar2tc@gmail.com <eduar2tc@gmail.com>
2022-04-13 21:23:36 -0500

    variables

---
+++ src/variables.kt
@@ -0,0 +1,16 @@
+/*Variables kotlin */
+fun main(args: Array<String>) {
+
+    var name = "Eduardo"
+    var isAswesome = true
+
+    println("Is " + name + " aswesome ? The answer is : " + isAswesome)
+
+    var a = 3
+    var b = 6
+    println(a + b)
+
+    //Mutability
+    isAswesome = false
+
+}
