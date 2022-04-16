9770bdbe4a0fb6cb4dc6b77fcbbdcbacb2657779
eduar2tc@gmail.com <eduar2tc@gmail.com>
2022-04-16 01:43:35 -0500

    operators in numbers

---
+++ src/Numbers.kt
@@ -0,0 +1,15 @@
+fun main( arg: Array<String> ) {
+    val a = 12
+    val b = 25.3
+
+    //Operators
+    /* + - / * */
+    println( a + b )
+    println( a - b )
+    println( a / b )
+    println( a * b )
+    //Module
+    val c = 12
+    val d = 25
+    println( d % c )
+}
